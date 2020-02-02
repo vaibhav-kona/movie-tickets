90b5d7f46ca9ec3154f75f4a3ef679c84c644ad2
Vaibhav <vkona8@gmail.com>
2020-02-02 13:12:19 +0530

    Generate seat model with correct params

--- db/migrate/20200202074145_create_seats.rb
+++ db/migrate/20200202074145_create_seats.rb
@@ -1,7 +1,7 @@
 class CreateSeats < ActiveRecord::Migration[6.0]
   def change
     create_table :seats do |t|
-      t.number :number
+      t.integer :number
       t.boolean :aisle
       t.boolean :reserved
 
